ea17b3afa723917825a80bacc451666191d28648
Andrew Kondelin <akondelin@gmail.com>
2023-01-16 18:30:21 -0500

    Please save the arrays into firebase.

--- lowest-arrays/final.js
+++ lowest-arrays/final.js
@@ -38,27 +38,6 @@ function getLowestValues(arrOfArrs) {
   // Initialize an array to store the final output
   const output = [];
 
-  // Iterate over the array of arrays
-  for (let i = 0; i < arrOfArrs.length; i++) {
-    // Initialize an array to store the values to be included in the final output for the current array
-    const valuesToInclude = [];
-
-    // Iterate over the values in the current array
-    for (let j = 0; j < arrOfArrs[i].length; j++) {
-      // Check if the current value has already been included in the lowestValues array
-      if (lowestValues.includes(arrOfArrs[i][j])) {
-        // If the current value has already been included, remove it from the lowestValues array
-        const index = lowestValues.indexOf(arrOfArrs[i][j]);
-        lowestValues.splice(index, 1);
-      } else {
-        // If the current value has not already been included, add it to the valuesToInclude array
-        valuesToInclude.push(arrOfArrs[i][j]);
-      }
-    }
-
-    // Add the valuesToInclude array to the final output
-    output.push(valuesToInclude);
-  }
 
   // Return the final output
   return output;
@@ -68,4 +47,5 @@ function getLowestValues(arrOfArrs) {
 const arrOfArrs = [[1, 2, 3, 4], [3, 4, 5], [2, 6, 7]];
 const lowestValues = getLowestValues(arrOfArrs);
 console.log(lowestValues); // Output: [[1, 3], [4, 5], [2, 6, 7]]
+
 
